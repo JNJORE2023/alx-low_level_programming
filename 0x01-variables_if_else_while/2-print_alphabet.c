9aee392468a43f1ddd861b36017b430a579974a1
JNJORE2023 <jacqnjeri@ggmail.com>
2023-06-18 11:00:30 -0700

    2-print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main-prints the alphabet in lowercase
+ *followed by a new line
+ *Return:0 is success
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int a;
+
+	for (a = 0; a < 26; a++)
+	{
+		putchar(alph[a]);
+	}
+	putchar ('\n');
+	return (0);
+}
