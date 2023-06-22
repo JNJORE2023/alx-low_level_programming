7e7f163e7f35caa87112f7b5f575d7c5aa879a9a
JNJORE2023 <jacqnjeri@ggmail.com>
2023-06-22 00:27:21 -0700

    postitive_or_negative

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,18 @@
 #include "main.h"
 /**
- * main - program that prints _putchar
- *
- * Return 0 is success
- *\
-
- int main(void)
- {
- _putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+ * main - the start point
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - testing function for negative or positive
+ * Return 0 is always success
+ */
+int main(void)
+{
+	int i;
+	i=0
+	positive_or_negative(i);
+	return (0);
+}
