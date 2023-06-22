76b57b4c5f177f34e93c3efc80e28836ccb3e04e
JNJORE2023 <jacqnjeri@ggmail.com>
2023-06-22 00:33:13 -0700

    1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - code that causes output on an infinite loop
+* Return: 0
+*/
+int main(void)
+{
+int i;
+printf("Infinite loop incoming :(\n");
+i = 0;
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
