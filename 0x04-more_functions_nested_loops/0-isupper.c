6188e8423b706d76ba1a204be9c24a774593240c
JNJORE2023 <jacqnjeri@ggmail.com>
2023-06-22 12:15:06 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_isupper - checking on lowercase characters
+*@c:character to check
+*Return 0 for anything and 1 for uppercase
+*/
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z'
+			return (1);
+	else
+	return (0);
+}
+
