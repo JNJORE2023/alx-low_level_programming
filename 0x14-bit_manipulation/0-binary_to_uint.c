0e1b9732a63d20b2220b90d39319b3da08293f7e
JNJORE2023 <jacqnjeri@gmail.com>
2023-08-03 13:20:13 -0700

    0-binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,19 +6,16 @@
  */
 unsigned int binary_to_uint(const char *binary_str)
 {
-    int i;
-    unsigned int decimal_val = 0;
+	int i;
+	unsigned int decimal_val = 0;
 
-    if (!binary_str)
-        return (0);
-
-    for (i = 0; binary_str[i]; i++)
-    {
-        if (binary_str[i] < '0' || binary_str[i] > '1')
-            return (0);
-        decimal_val = 2 * decimal_val + (binary_str[i] - '0');
-    }
-
-    return (decimal_val);
+	if (!binary_str)
+		return (0);
+for (i = 0; binary_str[i]; i++)
 }
-
+if (binary_str[i] < '0' || binary_str[i] > '1')
+return (0);
+decimal_val = 2 * decimal_val + (binary_str[i] - '0');
+}
+return (decimal_val);
+}
