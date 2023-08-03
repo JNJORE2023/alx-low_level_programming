8ca551c08b5104df300be72dbe26a561100c253c
JNJORE2023 <jacqnjeri@gmail.com>
2023-08-03 13:28:44 -0700

    1--print

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+/**
+ * print_binary - Prints the binary representation of a decimal number.
+ * @num: The decimal number to be printed in binary.
+ */
+void print_binary(unsigned long int num)
+{
+	int i, count = 0;
+	unsigned long int current;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = num >> i;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
+
