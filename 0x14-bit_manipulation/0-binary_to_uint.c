00e759938d7cbccc8b668ef326be60c46fed7567
JNJORE2023 <jacqnjeri@gmail.com>
2023-08-03 13:11:30 -0700

    0-binary

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @binary_str: string containing the binary number
+ *
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *binary_str)
+{
+    int i;
+    unsigned int decimal_val = 0;
+
+    if (!binary_str)
+        return (0);
+
+    for (i = 0; binary_str[i]; i++)
+    {
+        if (binary_str[i] < '0' || binary_str[i] > '1')
+            return (0);
+        decimal_val = 2 * decimal_val + (binary_str[i] - '0');
+    }
+
+    return (decimal_val);
+}
+
