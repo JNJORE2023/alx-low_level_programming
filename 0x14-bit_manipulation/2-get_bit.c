74171c339355b7a2ff4b02e77745aba3babf9b38
JNJORE2023 <jacqnjeri@gmail.com>
2023-08-03 13:43:52 -0700

    2-get

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,7 +3,8 @@
  * @num: The decimal number to search for the bit.
  * @index: The index of the bit to retrieve.
  *
- * Return: The value of the bit at the given index, or -1 if the index is out of range.
+ * Return: The value of the bit at the given index,
+ *or -1 if the index is out of range.
  */
 int get_bit(unsigned long int num, unsigned int index)
 {
