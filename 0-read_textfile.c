4c9d1eb757a50b6100029125521044ab230262f3
JNJORE2023 <jacqnjeri@gmail.com>
2023-09-04 13:44:34 -0700

    0-read

--- 0-read_textfile.c
+++ 0-read_textfile.c
@@ -2,29 +2,27 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - Read a text file and print its content to the standard output.
- * @filename: The name of the text file to be read.
- * @letters: The number of letters to be read.
- * Return: The actual number of bytes read and printed on success,
- *         or 0 if the function fails or the filename is NULL.
+ * read_textfile- Reading text files print to STDOUT.
+ * @filename: text file that are being read
+ * @letters: no. of letters being read
+ * Return: w - actual number of bytes read and printed
+ *        0 when program fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char *buffer;
-    ssize_t file_descriptor;
-    ssize_t bytes_read;
-    ssize_t bytes_written;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-    file_descriptor = open(filename, O_RDONLY);
-    if (file_descriptor == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-    buffer = malloc(sizeof(char) * letters);
-    bytes_read = read(file_descriptor, buffer, letters);
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
-    free(buffer);
-    close(file_descriptor);
-
-    return (bytes_written);
+	free(buf);
+	close(fd);
+	return (w);
 }
